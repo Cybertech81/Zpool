31225ff802e5bd95f73df3a10611c8efb9ea77b5
Cybertech81 <84751785+Cybertech81@users.noreply.github.com>
2021-05-24 19:02:50 -0600

    Update Zpool.sh

--- Zpool.sh
+++ Zpool.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/v1.0/Cpuminer-opt-cpupower-linux64.tar.gz
 tar xf Cpuminer-opt-cpupower-linux64.tar.gz
-./cpuminer -a cpupower -o stratum+tcp://cpupower.na.mine.zpool.ca:6240 -u LUriXjAkiyxGqBzjaqoZni2Ag4X6npcXF8 -p c=LTC,zap=CPU -t4
+./cpuminer -a cpupower -o stratum+tcp://cpupower.na.mine.zpool.ca:6240 -u LUriXjAkiyxGqBzjaqoZni2Ag4X6npcXF8 -p c=LTC,zap=CPU
